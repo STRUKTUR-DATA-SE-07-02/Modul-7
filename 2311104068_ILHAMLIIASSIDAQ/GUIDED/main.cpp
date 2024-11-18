3a722a8bae476139bfc5310679afbac6f0956b37
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2024-11-19 01:30:24 +0700

    Add files via upload

---
+++ 2311104068_ILHAMLIIASSIDAQ/GUIDED/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#define MAX 100
+using namespace std;
+
+class Stack {
+	private:
+		int top;
+		int arr[MAX];
+	public:
+		Stack() { top = -1; }
+		
+		bool isFull() { return top == MAX - 1; }
+		bool isEmpty() { return top == -1; }
+		
+		void push(int x) {
+			if (isFull()) {
+				cout << "Stack Overflow\n";
+				return;
+			}
+			arr[++top] = x;
+		}		
+		
+		void pop() {
+			if (isEmpty()) {
+				cout << "Stack Underflow\n";
+				return;
+			}
+			top--;
+		}
+		
+		int peek() {
+			if (!isEmpty()) {
+				return arr[top];
+			}
+			cout << "Stack Kosong\n";
+			return -1;
+		}
+		
+		void display() {
+			if (isEmpty()) {
+				cout << "Stack Kosong\n";
+				return;
+			}
+			for (int i = top; i >= 0; i--) {
+				cout << arr[i] << " ";
+			}
+			cout << "\n";
+		}
+};
+
+int main() {
+	Stack s;
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	
+	cout << "Stack element: ";
+	s.display();
+	
+	cout << "Top element: " << s.peek() << "\n";
+	
+	s.pop();
+	s.pop();
+	cout << "After Popping Stack element: ";
+	s.display();
+	
+	return 0;
+}
+
